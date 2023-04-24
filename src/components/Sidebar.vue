c764bc76f09b5794443f4fc23c8ec31fec2f052a
OG'ABEK <shomoyxhacker007@mail.ru>
2023-04-24 15:14:13 +0500

    feat: sidebar active class

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -7,69 +7,69 @@ import UserListIcon from './Icons/UserListIcon.vue';
 </script>
 
 <template>
-  <div class="fixed block h-screen bg-gray-900 text-white w-80">
+  <div class="fixed block h-screen bg-gray-900 w-80 text-gray-400">
     <div class="bg-gray-600 h-20 flex items-center justify-center">
       <img src="/images/alpha-sport-urgench-logo.png" class="h-14 w-auto" alt="#" />
     </div>
     <ul class="relative space-y-1 py-14">
-      <router-link to="/dashboard"
+      <router-link to="/dashboard" active-class="active"
         class="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 cursor-pointer transition-colors duration-300">
         <div class="-ml-1.5 w-3 h-10 rounded-xl bg-gray-900"></div>
-        <div class="px-10 flex items-center text-white space-x-5">
-          <HouseIcon class="w-7 h-7 text-gray-400" />
+        <div class="px-10 flex items-center space-x-5">
+          <HouseIcon class="w-7 h-7" />
           <p>{{ $t('mainPage') }}</p>
         </div>
       </router-link>
-      <router-link to="/tariffs"
+      <router-link to="/tariffs" active-class="active"
         class="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 cursor-pointer transition-colors duration-300">
         <div class="-ml-1.5 w-3 h-10 rounded-xl bg-gray-900"></div>
-        <div class="px-10 flex items-center text-white space-x-5">
-          <FolderUserIcon class="w-7 h-7 text-gray-400" />
+        <div class="px-10 flex items-center space-x-5">
+          <FolderUserIcon class="w-7 h-7" />
           <p>{{ $t('customerTariffs') }}</p>
         </div>
       </router-link>
-      <router-link to="/trainer-tariffs"
+      <router-link to="/trainer-tariffs" active-class="active"
         class="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 cursor-pointer transition-colors duration-300">
         <div class="-ml-1.5 w-3 h-10 rounded-xl bg-gray-900"></div>
-        <div class="px-10 flex items-center text-white space-x-5">
-          <FolderUserIcon class="w-7 h-7 text-gray-400" />
+        <div class="px-10 flex items-center space-x-5">
+          <FolderUserIcon class="w-7 h-7" />
           <p>{{ $t('customerTrainerTariffs') }}</p>
         </div>
       </router-link>
-      <router-link to="/customers"
+      <router-link to="/customers" active-class="active"
         class="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 cursor-pointer transition-colors duration-300">
         <div class="-ml-1.5 w-3 h-10 rounded-xl bg-gray-900"></div>
-        <div class="px-10 flex items-center text-white space-x-5">
-          <UsersThreeIcon class="w-7 h-7 text-gray-400" />
+        <div class="px-10 flex items-center space-x-5">
+          <UsersThreeIcon class="w-7 h-7" />
           <p>{{ $t('customers') }}</p>
         </div>
       </router-link>
-      <router-link to="/services"
+      <router-link to="/services" active-class="active"
         class="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 cursor-pointer transition-colors duration-300">
         <div class="-ml-1.5 w-3 h-10 rounded-xl bg-gray-900"></div>
-        <div class="px-10 flex items-center text-white space-x-5">
-          <FolderOpenIcon class="w-7 h-7 text-gray-400" />
+        <div class="px-10 flex items-center space-x-5">
+          <FolderOpenIcon class="w-7 h-7" />
           <p>{{ $t('services') }}</p>
         </div>
       </router-link>
-      <router-link to="/trainer-services"
+      <router-link to="/trainer-services" active-class="active"
         class="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 cursor-pointer transition-colors duration-300">
         <div class="-ml-1.5 w-3 h-10 rounded-xl bg-gray-900"></div>
-        <div class="px-10 flex items-center text-white space-x-5">
-          <FolderOpenIcon class="w-7 h-7 text-gray-400" />
+        <div class="px-10 flex items-center space-x-5">
+          <FolderOpenIcon class="w-7 h-7" />
           <p>{{ $t('trainerServices') }}</p>
         </div>
       </router-link>
-      <router-link to="/users"
+      <router-link to="/users" active-class="active"
         class="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 cursor-pointer transition-colors duration-300">
         <div class="-ml-1.5 w-3 h-10 rounded-xl bg-gray-900"></div>
-        <div class="px-10 flex items-center text-white space-x-5">
-          <UserListIcon class="w-7 h-7 text-gray-400" />
+        <div class="px-10 flex items-center space-x-5">
+          <UserListIcon class="w-7 h-7" />
           <p>{{ $t('users') }}</p>
         </div>
       </router-link>
     </ul>
-    <div class="absolute bottom-0 w-full mb-3 text-sm text-center text-gray-400 dark:text-gray-400">
+    <div class="absolute bottom-0 w-full mb-3 text-sm text-center dark:text-gray-400">
       Copyright &copy; {{ new Date().getFullYear() }} <a href="http://it-forelead.uz" class="hover:underline">IT-Forelead</a>. <br />
       All Rights Reserved.
     </div>
