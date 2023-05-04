de32cf8b00996c75dd6dbc3d88ac398d66a94ff1
Surojiddin <isurojiddin@gmail.com>
2023-05-04 12:51:12 +0500

    sidebar improved

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue';
 import decodeJwt, { parseJwt } from '../mixins/utils';
 import { useAuthStore } from '../store/auth.store';
 import { useSidebarStore } from '../store/sidebar.store';
+import { useRouter } from 'vue-router'
 import EnvelopeIcon from './Icons/EnvelopeIcon.vue';
 import FolderOpenIcon from './Icons/FolderOpenIcon.vue';
 import FolderUserIcon from './Icons/FolderUserIcon.vue';
@@ -13,6 +14,8 @@ import UserListIcon from './Icons/UserListIcon.vue';
 import UsersThreeIcon from './Icons/UsersThreeIcon.vue';
 import ChevronRightIcon from './Icons/ChevronRightIcon.vue';
 import FootPrintsIcon from './Icons/FootPrintsIcon.vue';
+
+const router = useRouter()
 
 const payload = ref({})
 
@@ -37,32 +40,34 @@ onMounted(() => {
     </div>
     <div class="relative space-y-1 py-14">
       <router-link to="/dashboard" active-class="active"
-        class="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 px-6 space-x-4 cursor-pointer transition-colors duration-300">
-        <div class="flex items-center justify-center rounded-xl w-10 h-10">
+        class="relative flex items-center h-10 w-full hover:bg-yellow-300/10 hover:text-yellow-200 py-7 font-medium space-x-4 cursor-pointer transition-colors duration-300">
+        <div class="w-1.5 h-10 rounded-r-xl first-child-bg-color mr-2"></div>
+        <div class="flex items-center justify-center rounded-xl w-10 h-10 second-child-bg-color">
           <HouseIcon class="w-6 h-6" />
         </div>
         <div>{{ $t('mainPage') }}</div>
       </router-link>
-      <div @click="useSidebarStore().toggleSubMenuForTariffs()" active-class="active"
-        class="relative flex items-center justify-between h-10 w-full hover:bg-yellow-500/10 py-7 px-6 cursor-pointer transition-colors duration-300">
-        <div class="flex items-center space-x-4">
-          <div class="flex items-center justify-center rounded-xl w-10 h-10">
+      <div @click="useSidebarStore().toggleSubMenuForTariffs()"
+        class="relative flex items-center justify-between h-10 w-full hover:bg-yellow-300/10 font-medium  py-7 cursor-pointer transition-colors duration-300">
+        <div class="flex items-center space-x-4" :class="router?.currentRoute?.value?.path === '/tariffs' || router?.currentRoute?.value?.path === '/trainer-tariffs' ? 'active' : ''">
+          <div class="w-1.5 h-10 rounded-r-xl mr-2 first-child-bg-color"></div>
+          <div class="flex items-center justify-center rounded-xl w-10 h-10 second-child-bg-color">
             <FolderUserIcon class="w-6 h-6" />
           </div>
           <div>{{ $t('tariffs') }}</div>
         </div>
-        <ChevronRightIcon class="w-5 h-5 transition-all duration-300" :class="{ 'rotate-90': isOpenSubMenuForTariffs }" />
+        <ChevronRightIcon class="w-5 h-5 transition-all duration-300 mx-4" :class="{ 'rotate-90': isOpenSubMenuForTariffs }" />
       </div>
       <div :class="{ hidden: !isOpenSubMenuForTariffs }" class="transition-all duration-300">
-        <router-link to="/tariffs" active-class="text-yellow-400"
-          class="flex items-center justify-between hover:bg-yellow-500/10 p-3 rounded-lg cursor-pointer pl-14">
+        <router-link to="/tariffs" active-class="active"
+          class="flex items-center justify-between hover:bg-yellow-300/10 hover:text-yellow-200 p-3 rounded-lg cursor-pointer pl-14">
           <div class="flex items-center space-x-2">
             <p>-</p>
             <p>{{ $t('customerTariff') }}</p>
           </div>
         </router-link>
-        <router-link to="/trainer-tariffs" active-class="text-yellow-400"
-          class="flex items-center justify-between hover:bg-yellow-500/10 p-3 rounded-lg cursor-pointer pl-14">
+        <router-link to="/trainer-tariffs" active-class="active"
+          class="flex items-center justify-between hover:bg-yellow-300/10 hover:text-yellow-200 p-3 rounded-lg cursor-pointer pl-14">
           <div class="flex items-center space-x-2">
             <p>-</p>
             <p>{{ $t('trainerTariffs') }}</p>
@@ -70,46 +75,51 @@ onMounted(() => {
         </router-link>
       </div>
       <router-link to="/payments" active-class="active"
-        class="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 px-6 space-x-4 cursor-pointer transition-colors duration-300">
-        <div class="flex items-center justify-center rounded-xl w-10 h-10">
+        class="relative flex items-center h-10 w-full hover:bg-yellow-300/10 hover:text-yellow-200 py-7 font-medium space-x-4 cursor-pointer transition-colors duration-300">
+        <div class="w-1.5 h-10 rounded-r-xl first-child-bg-color mr-2"></div>
+        <div class="flex items-center justify-center rounded-xl w-10 h-10 second-child-bg-color">
           <MoneyIcon class="w-6 h-6" />
         </div>
         <div>{{ $t('payments') }}</div>
       </router-link>
       <router-link to="/visits" active-class="active"
-        class="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 px-6 space-x-4 cursor-pointer transition-colors duration-300">
-        <div class="flex items-center justify-center rounded-xl w-10 h-10">
+        class="relative flex items-center h-10 w-full hover:bg-yellow-300/10 hover:text-yellow-200 py-7 font-medium space-x-4 cursor-pointer transition-colors duration-300">
+        <div class="w-1.5 h-10 rounded-r-xl first-child-bg-color mr-2"></div>
+        <div class="flex items-center justify-center rounded-xl w-10 h-10 second-child-bg-color">
           <FootPrintsIcon class="w-6 h-6" />
         </div>
         <div>{{ $t('visits') }}</div>
       </router-link>
       <router-link to="/customers" active-class="active"
-        class="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 px-6 space-x-4 cursor-pointer transition-colors duration-300">
-        <div class="flex items-center justify-center rounded-xl w-10 h-10">
+        class="relative flex items-center h-10 w-full hover:bg-yellow-300/10 hover:text-yellow-200 py-7 font-medium space-x-4 cursor-pointer transition-colors duration-300">
+        <div class="w-1.5 h-10 rounded-r-xl first-child-bg-color mr-2"></div>
+        <div class="flex items-center justify-center rounded-xl w-10 h-10 second-child-bg-color">
           <UsersThreeIcon class="w-6 h-6" />
         </div>
         <div>{{ $t('customers') }}</div>
       </router-link>
-      <div @click="useSidebarStore().toggleSubMenuForServices()" active-class="active"
-        class="relative flex items-center justify-between h-10 w-full hover:bg-yellow-500/10 py-7 px-6 space-x-4 cursor-pointer transition-colors duration-300">
-        <div class="flex items-center space-x-4">
-          <div class="flex items-center justify-center rounded-xl w-10 h-10">
+
+      <div @click="useSidebarStore().toggleSubMenuForServices()"
+        class="relative flex items-center justify-between h-10 w-full hover:bg-yellow-300/10 font-medium  py-7 cursor-pointer transition-colors duration-300">
+        <div class="flex items-center space-x-4" :class="router?.currentRoute?.value?.path === '/services' || router?.currentRoute?.value?.path === '/trainer-services' ? 'active' : ''">
+          <div class="w-1.5 h-10 rounded-r-xl mr-2 first-child-bg-color"></div>
+          <div class="flex items-center justify-center rounded-xl w-10 h-10 second-child-bg-color">
             <FolderOpenIcon class="w-6 h-6" />
           </div>
           <div>{{ $t('services') }}</div>
         </div>
-        <ChevronRightIcon class="w-5 h-5 transition-all duration-300" :class="{ 'rotate-90': isOpenSubMenuForServices }" />
+        <ChevronRightIcon class="w-5 h-5 transition-all duration-300 mx-4" :class="{ 'rotate-90': isOpenSubMenuForServices }" />
       </div>
       <div :class="{ hidden: !isOpenSubMenuForServices }" class="transition-all duration-300">
-        <router-link to="/services" active-class="text-yellow-400"
-          class="flex items-center justify-between hover:bg-yellow-500/10 p-3 rounded-lg cursor-pointer pl-14">
+        <router-link to="/services" active-class="active"
+          class="flex items-center justify-between hover:bg-yellow-300/10 hover:text-yellow-200 p-3 rounded-lg cursor-pointer pl-14">
           <div class="flex items-center space-x-2">
             <p>-</p>
             <p>{{ $t('services') }}</p>
           </div>
         </router-link>
-        <router-link to="/trainer-services" active-class="text-yellow-400"
-          class="flex items-center justify-between hover:bg-yellow-500/10 p-3 rounded-lg cursor-pointer pl-14">
+        <router-link to="/trainer-services" active-class="active"
+          class="flex items-center justify-between hover:bg-yellow-300/10 hover:text-yellow-200 p-3 rounded-lg cursor-pointer pl-14">
           <div class="flex items-center space-x-2">
             <p>-</p>
             <p>{{ $t('trainerServices') }}</p>
@@ -117,15 +127,17 @@ onMounted(() => {
         </router-link>
       </div>
       <router-link to="/users" active-class="active"
-        class="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 px-6 space-x-4 cursor-pointer transition-colors duration-300">
-        <div class="flex items-center justify-center rounded-xl w-10 h-10">
+        class="relative flex items-center h-10 w-full hover:bg-yellow-300/10 hover:text-yellow-200 py-7 font-medium space-x-4 cursor-pointer transition-colors duration-300">
+        <div class="w-1.5 h-10 rounded-r-xl first-child-bg-color mr-2"></div>
+        <div class="flex items-center justify-center rounded-xl w-10 h-10 second-child-bg-color">
           <UserListIcon class="w-6 h-6" />
         </div>
         <div>{{ $t('users') }}</div>
       </router-link>
-      <router-link to="/sms-messages" v-if="navigationGuard(['super_manager'])" active-class="active"
-        class="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 px-6 space-x-4 cursor-pointer transition-colors duration-300">
-        <div class="flex items-center justify-center rounded-xl w-10 h-10">
+      <router-link to="/sms-messages" active-class="active"
+        class="relative flex items-center h-10 w-full hover:bg-yellow-300/10 hover:text-yellow-200 py-7 font-medium space-x-4 cursor-pointer transition-colors duration-300">
+        <div class="w-1.5 h-10 rounded-r-xl first-child-bg-color mr-2"></div>
+        <div class="flex items-center justify-center rounded-xl w-10 h-10 second-child-bg-color">
           <EnvelopeIcon class="w-6 h-6" />
         </div>
         <div>{{ $t('smsMessages') }}</div>
