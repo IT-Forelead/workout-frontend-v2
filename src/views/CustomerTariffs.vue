6988dd6420354fe2e5c3bac04299f3f22f28c1b4
Surojiddin <isurojiddin@gmail.com>
2023-05-22 15:52:57 +0500

    tariff status improved

--- src/views/CustomerTariffs.vue
+++ src/views/CustomerTariffs.vue
@@ -179,7 +179,6 @@ const submitFilterData = () => {
               <th class="py-2 px-4 text-left">{{ $t('price') }}</th>
               <th class="py-2 px-4 text-center">{{ $t('monthlyVisit') }}</th>
               <th class="py-2 px-4 text-center">{{ $t('paymentStatus') }}</th>
-              <th class="py-2 px-4 text-center">{{ $t('status') }}</th>
               <th class="py-2 px-4 text-center">{{ $t('actions') }}</th>
             </tr>
           </thead>
