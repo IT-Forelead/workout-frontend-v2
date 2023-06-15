73de9e1a1abf42f0ca92bebc8ab1ecc0795212cb
surojiddin <isurojiddin@gmail.com>
2023-06-15 14:13:29 +0500

    small change

--- src/components/Modals/AddTariffWithPaymentModal.vue
+++ src/components/Modals/AddTariffWithPaymentModal.vue
@@ -5,26 +5,22 @@ import 'izitoast/dist/css/iziToast.min.css'
 import { useI18n } from 'vue-i18n'
 import { cleanObjectEmptyFields } from '../../mixins/utils'
 import CustomerTariffService from '../../services/customerTariff.service'
+import PaymentService from '../../services/payment.service.js'
 import { useDropdownStore } from '../../store/dropdown.store'
 import { useModalStore } from '../../store/modal.store'
-import XIcon from '../Icons/XIcon.vue'
-import SelectOptionCustomer from '../Inputs/SelectOptionCustomer.vue'
-import SelectOptionService from '../Inputs/SelectOptionService.vue'
 import Spinners270RingIcon from '../Icons/Spinners270RingIcon.vue'
-import PaymentService from '../../services/payment.service.js'
-import ChevronRightIcon from '../Icons/ChevronRightIcon.vue'
 import UserIcon from '../Icons/UserIcon.vue'
-import SelectOptionCustomerItem from '../Items/SelectOptionCustomerItem.vue'
-import SearchIcon from '../Icons/SearchIcon.vue'
+import XIcon from '../Icons/XIcon.vue'
+import SelectOptionService from '../Inputs/SelectOptionService.vue'
 
 const { t } = useI18n()
 
 const createdAt = ref('')
 
 const moneyConf = {
-   thousands: ' ',
-   suffix: ' UZS',
-   precision: 0,
+  thousands: ' ',
+  suffix: ' UZS',
+  precision: 0,
 }
 
 const cost = ref(0)
@@ -69,10 +65,10 @@ const submitServiceData = () => {
         createdAt: createdAt.value,
       })
     ).then((data) => {
-        notify.success({
-          message: t('customerTariffCreated'),
-        })
-        isLoading.value = false
+      notify.success({
+        message: t('customerTariffCreated'),
+      })
+      isLoading.value = false
       if (cost.value > 0) {
         PaymentService.createPayment(
           cleanObjectEmptyFields({
@@ -90,9 +86,8 @@ const submitServiceData = () => {
             })
           })
       }
-
       closeModal()
-      })
+    })
       .catch((err) => {
         notify.error({
           message: t('errorCreatingCustomerTariff'),
@@ -102,18 +97,15 @@ const submitServiceData = () => {
   }
 }
 </script>
-
-
-
 <template>
   <div v-if="useModalStore().isAddTariffWithPaymentModalOpen"
-       class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 backdrop-blur bg-gray-900/75 w-full max-h-screen md:inset-0 md:h-full">
+    class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 backdrop-blur bg-gray-900/75 w-full max-h-screen md:inset-0 md:h-full">
     <div class="relative p-4 w-full h-full max-w-xl md:h-auto left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
       <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
         <div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
           <div class="text-xl font-medium">{{ $t('addCustomerTariff') }}</div>
           <button @click="closeModal()"
-                  class="text-gray-600 bg-gray-100 hover:bg-gray-800 hover:text-gray-300 transition-all duration-300 rounded-full text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
+            class="text-gray-600 bg-gray-100 hover:bg-gray-800 hover:text-gray-300 transition-all duration-300 rounded-full text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
             <XIcon />
           </button>
         </div>
@@ -127,13 +119,12 @@ const submitServiceData = () => {
                   <UserIcon class="w-10 h-10 border-r rounded-lg p-2" />
                 </div>
                 <div v-if="useDropdownStore().selectCustomerOption"
-                     class="border-none focus:ring-0 outline-0 bg-gray-100 w-full text-lg rounded-r-lg pl-2 py-2 capitalize">
-                  {{ useDropdownStore().selectCustomerOption?.firstname + ' ' + useDropdownStore().selectCustomerOption?.lastname }}
+                  class="border-none focus:ring-0 outline-0 bg-gray-100 w-full text-lg rounded-r-lg pl-2 py-2 capitalize">
+                  {{ useDropdownStore().selectCustomerOption?.firstname + ' ' +
+                    useDropdownStore().selectCustomerOption?.lastname }}
                 </div>
-
               </label>
             </div>
-
           </div>
           <div>
             <label>{{ $t('service') }}</label>
@@ -142,22 +133,21 @@ const submitServiceData = () => {
           <div>
             <label for="createdAt">{{ $t('createdAt') }}</label>
             <input v-model="createdAt" type="datetime-local" id="createdAt"
-                   class="block border-none text-gray-500 bg-gray-100 rounded-lg w-full text-lg" />
+              class="block border-none text-gray-500 bg-gray-100 rounded-lg w-full text-lg" />
           </div>
           <div>
             <label for="price">{{ $t('price') }}</label>
             <money3 v-model="cost" v-bind="moneyConf" id="price"
-                    class="border-none text-right text-gray-500 bg-gray-100 rounded-lg w-full text-lg"> </money3>
+              class="border-none text-right text-gray-500 bg-gray-100 rounded-lg w-full text-lg"> </money3>
           </div>
         </div>
-
         <div class="flex items-center justify-end p-4 space-x-2 border-t dark:border-gray-600">
           <button @click="clearForm()"
-                  class="w-36 py-2 px-4 rounded-md text-white text-base bg-gray-600 cursor-pointer hover:bg-gray-800">
+            class="w-36 py-2 px-4 rounded-md text-white text-base bg-gray-600 cursor-pointer hover:bg-gray-800">
             {{ $t('reset') }}
           </button>
           <button v-if="!isLoading" @click="submitServiceData()"
-                  class="w-36 py-2 px-4 rounded-md text-white text-base bg-blue-600 cursor-pointer hover:bg-blue-800">
+            class="w-36 py-2 px-4 rounded-md text-white text-base bg-blue-600 cursor-pointer hover:bg-blue-800">
             {{ $t('save') }}
           </button>
           <button v-else class="w-36 p-2 rounded-md text-white text-base bg-blue-500 select-none">
@@ -172,5 +162,3 @@ const submitServiceData = () => {
     </div>
   </div>
 </template>
-
-
