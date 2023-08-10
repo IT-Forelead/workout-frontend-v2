979a510b8b515c0741ae61b6a4c1f7979ba5f756
matkarimov <matkarim1099@gmail.com>
2023-08-10 15:27:12 +0500

    Delete modal changed to dark mode

--- src/components/Modals/DeleteServiceModal.vue
+++ src/components/Modals/DeleteServiceModal.vue
@@ -79,44 +79,44 @@ const monthlyVisitTranslate = (n) => {
   <div v-if="useModalStore().isDeleteServiceModalOpen"
     class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 backdrop-blur bg-gray-900/75 w-full max-h-screen md:inset-0 md:h-full">
     <div class="relative max-w-xl p-4 w-full h-full md:h-auto left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-      <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+      <div class="relative bg-white rounded-lg shadow dark:bg-gray-800">
         <div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
-          <div class="text-xl font-medium">{{ $t('deleteData') }}</div>
+          <div class="text-xl font-medium dark:text-white">{{ $t('deleteData') }}</div>
           <button @click="closeModal()"
-            class="text-gray-600 bg-gray-100 hover:bg-gray-800 hover:text-gray-300 transition-all duration-300 rounded-full text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
+            class="text-gray-600 bg-gray-100 hover:bg-gray-800 hover:text-gray-300 transition-all duration-300 rounded-full text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white dark:text-gray-300 dark:bg-gray-900">
             <XIcon />
           </button>
         </div>
         <div class="py-4 px-6 text-center space-y-6">
           <div>
             <div class="flex items-center space-x-2">
-              <div class="text-base text-gray-500">{{ $t('serviceName') }}:</div>
-              <div class="text-lg text-gray-700">{{ selectedService?.name }}
+              <div class="text-base text-gray-500 dark:text-white">{{ $t('serviceName') }}:</div>
+              <div class="text-lg text-gray-700 dark:text-gray-300">{{ selectedService?.name }}
               </div>
             </div>
             <div class="flex items-center space-x-2">
-              <div class="text-base text-gray-500">{{ $t('serviceType') }}:</div>
-              <div class="text-lg text-gray-700">{{ serviceTypeTranslate(selectedService?.serviceType) }}</div>
+              <div class="text-base text-gray-500 dark:text-white">{{ $t('serviceType') }}:</div>
+              <div class="text-lg text-gray-700 dark:text-gray-300">{{ serviceTypeTranslate(selectedService?.serviceType) }}</div>
             </div>
             <div class="flex items-center space-x-2">
-              <div class="text-base text-gray-500">{{ $t('duration') }}:</div>
-              <div class="text-lg text-gray-700">{{ durationDayTranslate(selectedService?.durationDay) }}</div>
+              <div class="text-base text-gray-500 dark:text-white">{{ $t('duration') }}:</div>
+              <div class="text-lg text-gray-700 dark:text-gray-300">{{ durationDayTranslate(selectedService?.durationDay) }}</div>
             </div>
             <div class="flex items-center space-x-2">
-              <div class="text-base text-gray-500">{{ $t('monthlyVisit') }}:</div>
-              <div class="text-lg text-gray-700">{{ monthlyVisitTranslate(selectedService?.monthlyVisit) }}</div>
+              <div class="text-base text-gray-500 dark:text-white">{{ $t('monthlyVisit') }}:</div>
+              <div class="text-lg text-gray-700 dark:text-gray-300">{{ monthlyVisitTranslate(selectedService?.monthlyVisit) }}</div>
             </div>
             <div class="flex items-center space-x-2">
-              <div class="text-base text-gray-500">{{ $t('priceForMale') }}:</div>
-              <div class="text-lg text-gray-700">{{ useMoneyFormatter(selectedService?.priceForMale) }}</div>
+              <div class="text-base text-gray-500 dark:text-white">{{ $t('priceForMale') }}:</div>
+              <div class="text-lg text-gray-700 dark:text-gray-300">{{ useMoneyFormatter(selectedService?.priceForMale) }}</div>
             </div>
             <div class="flex items-center space-x-2">
-              <div class="text-base text-gray-500">{{ $t('priceForFemale') }}:</div>
-              <div class="text-lg text-gray-700">{{ useMoneyFormatter(selectedService?.priceForFemale) }}</div>
+              <div class="text-base text-gray-500 dark:text-white">{{ $t('priceForFemale') }}:</div>
+              <div class="text-lg text-gray-700 dark:text-gray-300">{{ useMoneyFormatter(selectedService?.priceForFemale) }}</div>
             </div>
           </div>
           <h3
-            class="flex items-center justify-center mb-5 text-lg font-normal text-red-500 dark:text-gray-400 text-center">
+            class="flex items-center justify-center mb-5 text-lg font-normal text-red-500 dark:text-gray-300 text-center">
             <WarningCircleIcon class="w-7 h-7 mr-2" />
             {{ $t('doYouDelete') }}
           </h3>
@@ -125,7 +125,7 @@ const monthlyVisitTranslate = (n) => {
             {{ $t('yesImSure') }}
           </button>
           <button @click="closeModal()" type="button"
-            class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+            class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-600">
             {{ $t('noCancel') }}
           </button>
         </div>
